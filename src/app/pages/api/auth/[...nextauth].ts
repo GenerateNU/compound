f955a4bb2d23881593ce83c46b123f635a6d345e
Mario Gonzalez <MGonzalez@StateStreet.com>
2023-09-15 17:31:25 -0400

    modify nextauth.ts and install next auth

--- src/app/pages/api/auth/[...nextauth].ts
+++ src/app/pages/api/auth/[...nextauth].ts
@@ -1,22 +1,17 @@
-// import persistentUserInstance from "lib/userInstance";
-// import NextAuth, { NextAuthOptions } from "next-auth";
-// import GoogleProvider from "next-auth/providers/google";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-// export const authOptions: NextAuthOptions = {
-//   secret: process.env.JWT_SECRET,
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     }),
-//   ],
-//   callbacks: {
-//     async signIn({ user }) {
-//       persistentUserInstance.signUp({ name: user.name, email: user.email });
+export const authOptions: NextAuthOptions = {
+  secret: process.env.JWT_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+  }
 
-//       return true;
-//     },
-//   },
-// };
+};
 
-// export default NextAuth(authOptions);
+export default NextAuth(authOptions);
