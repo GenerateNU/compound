072415277332fd1b128233080135bcf279111e7e
Neha <ravuri.n@northeastern.edu>
2023-09-20 11:27:18 -0400

    intake test endpoint

---
+++ src/models/intakeTest.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+import { JsonObject, PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+
+type TestEntry = {
+    topic: string;
+    score: number;
+}
+
+interface IntakeTestData {
+  userId: number;
+  intakeTests: Array<TestEntry>
+  [key:string] : any;
+}
+const UNIQUE_CONSTRAINT_ERROR_CODE = "P2002";
+
+export default class IntakeTest {
+    constructor(private readonly intakeDB: PrismaClient["intakeTest"]) {}
+
+    public async takeTest(data: IntakeTestData) {
+        try {
+            data.intakeTests.forEach((value) => {
+                const dataEntry = {
+                    userId: data.userId,
+                    topic:value.topic,
+                    score:value.score,
+                    createdAt: new Date(),
+                }
+                this.intakeDB.create({data: dataEntry});
+            })
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === UNIQUE_CONSTRAINT_ERROR_CODE) {
+                    const parseErrorMessage = error.message.split("`");
+                    const failedOn = parseErrorMessage[parseErrorMessage.length - 2];
+                    throw new Error("Unique Constraint Violation Failed on " + failedOn);
+                }
+            } else {
+                throw error;
+            }
+        }
+    }
+
+}
