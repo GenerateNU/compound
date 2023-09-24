f61268a4c92a0d7206073aec8fb43bff7fda6c46
TylerSchaefer4 <93676493+TylerSchaefer4@users.noreply.github.com>
2023-09-24 12:40:18 -0400

    fixes

--- src/models/users.ts
+++ src/models/users.ts
@@ -46,66 +46,9 @@ export default class Users {
 
   private setFinancialDefaultsBasedOnDemographics(data: UserData): UserData {
     if (!data.employmentStatus || !data.householdIncome || !data.livingStatus) {
-      // Set defaults based on age https://www.capitalone.com/learn-grow/money-management/what-is-the-average-salary-in-the-us-by-age/
-      if (data.age) {
-        if (data.age < 25) {
-          data.employmentStatus = data.employmentStatus || "Student";
-          data.householdIncome = data.householdIncome || 30000;
-          data.livingStatus = data.livingStatus || "With parents";
-        } else if (data.age >= 25 && data.age < 35) {
-          data.employmentStatus = data.employmentStatus || "Entry Level Job";
-          data.householdIncome = data.householdIncome || 54184;
-          data.livingStatus = data.livingStatus || "Renting";
-        } else if (data.age >= 35) {
-          data.employmentStatus = data.employmentStatus || "Mid-Level Job";
-          data.householdIncome = data.householdIncome || 64116;
-          data.livingStatus = data.livingStatus || "Own Home";
-        }
-      }
-
-      // Set defaults based on ethnicity from 2021 US Census https://www.pgpf.org/blog/2023/02/income-and-wealth-in-the-united-states-an-overview-of-recent-data
-      if (data.ethnicity) {
-        if (data.ethnicity === "White") {
-          data.householdIncome = data.householdIncome || 77999;
-        } else if (data.ethnicity === "Black") {
-          data.householdIncome = data.householdIncome || 48297;
-        } else if (data.ethnicity === "Asian") {
-          data.householdIncome = data.householdIncome || 101418;
-        } else if (data.ethnicity === "Hispanic") {
-          data.householdIncome = data.householdIncome || 57981;
-        }
-      }
-
-      // Set defaults based on education https://www.capitalone.com/learn-grow/money-management/what-is-the-average-salary-in-the-us-by-age/
-      if (data.education) {
-        if (data.education === "High School") {
-          data.employmentStatus = data.employmentStatus || "???";
-          data.householdIncome = data.householdIncome || 46228;
-        } else if (data.education === "Bachelor's Degree") {
-          data.employmentStatus = data.employmentStatus || "???";
-          data.householdIncome = data.householdIncome || 82576;
-        } else if (data.education === "Some college or associate degree") {
-          data.employmentStatus = data.employmentStatus || "???";
-          data.householdIncome = data.householdIncome || 52520;
-        } else if (data.education === "No highschool diploma") {
-          data.employmentStatus = data.employmentStatus || "???";
-          data.householdIncome = data.householdIncome || 37076;
-        } else if (data.education === "Master's Degree") {
-          data.employmentStatus = data.employmentStatus || "???";
-          data.householdIncome = data.householdIncome || 115000;
-        }
-      }
-
-      //https://www.pewresearch.org/short-reads/2023/03/01/gender-pay-gap-facts/#:~:text=The%20gender%20gap%20in%20pay,%2D%20and%20part%2Dtime%20workers.
-      if (data.gender) {
-        if (data.gender === "Male") {
-          data.householdIncome =
-            data.householdIncome || data.householdIncome * 1.17;
-        } else if (data.gender === "Female") {
-          data.householdIncome =
-            data.householdIncome || data.householdIncome * 0.83;
-        }
-      }
+      data.employmentStatus = "employed";
+      data.householdIncome = 50000;
+      data.livingStatus = "???";
     }
     return data;
   }
