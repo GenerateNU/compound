55ade25c7ff4aed2a2a1a8208cf47be11ff69dae
TylerSchaefer4 <93676493+TylerSchaefer4@users.noreply.github.com>
2023-10-31 11:19:24 -0400

    asdasd

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,7 +1,12 @@
+import ProfileComponent from "@/components/ProfileComponent";
+import ProfileSetup from "@/components/ProfileSetup";
+import SkipSection from "@/components/Skip";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Profile Page!</h1>
-    </main>
+    <div>
+      <ProfileSetup />
+      <ProfileComponent />
+      {/* <SkipSection /> */}
+    </div>
   );
 }
