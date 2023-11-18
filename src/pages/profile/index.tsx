5e5d2a31a839dc0572f32a04f7a4076bbcf0dcaf
TylerSchaefer4 <93676493+TylerSchaefer4@users.noreply.github.com>
2023-11-18 15:14:46 -0500

    fix

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,14 +1,3 @@
-
 export default function Home() {
-  return (
-    <div>
-      <ProfileCustomization></ProfileCustomization>
-      {/* <ProfileSetup />
-      <ProfileComponent />
-      <ProgressComponent /> */}
-      {/* <SkipSection /> */}
-      {/* <Dashboard /> */}
-      {/* <Dashboard /> */}
-    </div>
-  );
+  return <div></div>;
 }
