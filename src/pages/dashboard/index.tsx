bfc6d3f5368182d697e4e7d34d89de8d9378aa19
Dylan Pham <43.dpham@gmail.com>
2023-12-07 14:01:33 -0500

    redirect to login when not authenticated

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
 import UpdatedComponent from "@/components/PostDashboard";
+import { useRouter } from "next/router";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (localStorage.getItem("email") === "") {
+      alert(
+        "Must be authenticated to view this page. Redirecting to login / sign up..."
+      );
+      router.push("/account");
+    }
+  }, []);
+
   return (
     <div>
       <UpdatedComponent />
