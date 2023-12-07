84f45da9a0502a5152dfb31079676bc06846821e
Dylan Pham <43.dpham@gmail.com>
2023-12-07 04:09:08 -0500

    bruh

--- lib/CompoundTypes.ts
+++ lib/CompoundTypes.ts
@@ -1,35 +1,35 @@
-export interface UserData {
-    email: string;
-    phoneNumber: string;
-    firstName: string;
-    lastName: string;
-    dob: Date;
-    [key: string]: any;
-  }
+// export interface UserData {
+//     email: string;
+//     phoneNumber: string;
+//     firstName: string;
+//     lastName: string;
+//     dob: Date;
+//     [key: string]: any;
+//   }
 
 
-export type UserReturnType = {
-    email: string;
-    id: number;
-    firstName?: string;
-    lastName?: string;
-    isAdmin?: string;
-  };
+// export type UserReturnType = {
+//     email: string;
+//     id: number;
+//     firstName?: string;
+//     lastName?: string;
+//     isAdmin?: string;
+//   };
 
-export type Message = {
-    message: string;
-  };
+// export type Message = {
+//     message: string;
+//   };
 
-export type Quizzes = {
-    id : number;
-    questions : string;
-    passed : boolean;
-    score : number;
-}
+// export type Quizzes = {
+//     id : number;
+//     questions : string;
+//     passed : boolean;
+//     score : number;
+// }
 
-export type AnswersType = string[] | null;
+// export type AnswersType = string[] | null;
 
-export type ExamSubmissionResponse = {
-  moduleID: number;
-  score: number;
-}
+// export type ExamSubmissionResponse = {
+//   moduleID: number;
+//   score: number;
+// }
